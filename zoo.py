551745b0f24e10245124a02db2600adcf9443c38
Kaiane <kaiane.cordeiro@sou.inteli.edu.br>
2024-05-03 22:24:41 -0300

    adiciona estrutura de pastas

---
+++ zoo.py
@@ -0,0 +1,8 @@
+# Criação de Animais com nome, espécie e nível de felicidade.
+
+
+# Criação de Recintos para abrigar os animais, contendo um ou mais animais da mesma espécie, sendo estes bem ou mal cuidados. Pense em funções para alterar os recintos.
+
+# Alimentar os Animais, sendo que isso irá tornar os animais mais ou menos felizes.
+
+# Receber visitantes - Esta função irá permitir que os jogadores ganhem dinheiro com base no número de visitantes que o zoológico atrai. Visitantes são atraídos por animais felizes e recintos bem cuidados.
